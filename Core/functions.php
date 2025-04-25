c6c728f5a589aac709bcb67a2434b20e5c1d7a25
farsewin <124394724+farsewin@users.noreply.github.com>
2025-04-25 20:29:36 +0100

    Initial commit

---
+++ Core/functions.php
@@ -0,0 +1,68 @@
+<?php
+
+use Core\Response;
+
+function dd($value)
+{
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+
+    die();
+}
+
+function urlIs($value)
+{
+    return $_SERVER['REQUEST_URI'] === $value;
+}
+
+function abort($code = 404)
+{
+    http_response_code($code);
+
+    require base_path("views/{$code}.php");
+
+    die();
+}
+
+function authorize($condition, $status = Response::FORBIDDEN)
+{
+    if (! $condition) {
+        abort($status);
+    }
+
+    return true;
+}
+
+function base_path($path)
+{
+    return BASE_PATH . $path;
+}
+
+function generateCsrfToken() {
+    if (empty($_SESSION['csrf_token'])) {
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    }
+    return $_SESSION['csrf_token'];
+}
+
+function validateCsrfToken($token) {
+    return isset($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $token);
+}
+
+function view($path, $attributes = []) {
+    $attributes['csrf_token'] = generateCsrfToken();
+    extract($attributes);
+    require base_path('views/' . $path);
+}
+
+function redirect($path)
+{
+    header("location: {$path}");
+    exit();
+}
+
+function old($key, $default = '')
+{
+    return Core\Session::get('old')[$key] ?? $default;
+}
